23ed775edf152afa1ae11fd336bc2b86808f416d
yashkhade <ykhade@nevada.unr.edu>
2019-07-14 18:26:55 -0700

    Completed StringUtil

--- src/com/yashkhade/StringUtil.java
+++ src/com/yashkhade/StringUtil.java
@@ -11,9 +11,14 @@ public class StringUtil {
             /* Applies Sha256 to our input */
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
             StringBuilder hexString = new StringBuilder();
-            for (byte elem:)
+            for (byte elem : hash) {
+                String hex = Integer.toHexString(0xff & elem);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-
-
 }

2dd9acc2fdc08b87d0886c7e4a0405a4637433b5
yashkhade <ykhade@nevada.unr.edu>
2019-07-18 14:40:16 -0700

    Were mining now

--- src/com/yashkhade/StringUtil.java
+++ src/com/yashkhade/StringUtil.java
@@ -2,6 +2,9 @@ package com.yashkhade;
 
 import java.security.MessageDigest;
 
+import com.google.gson.GsonBuilder;
+
+
 
 
 
@@ -22,6 +25,16 @@ public class StringUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //Short hand helper to turn Object into a json string
+    public static String getJson(Object o) {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+    }
+
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
     }
 
 }
